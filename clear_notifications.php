4c202aef1349b3c0a69926762bb2246cf908df22
Gabe Clevin K <cyano4400@gmail.com>
2026-06-27 19:24:32 -0300

    Create clear_notifications.php

---
+++ clear_notifications.php
@@ -0,0 +1,37 @@
+<?php
+
+header("Content-Type: application/json");
+
+require_once "init.php";
+
+$token=$_POST['token'] ?? '';
+
+$stmt=$db->prepare("
+SELECT user_id
+FROM sessions
+WHERE token=?
+AND expires>?
+");
+
+$stmt->execute([
+$token,
+time()
+]);
+
+$user=$stmt->fetch();
+
+if(!$user)
+die(json_encode([
+"success"=>false
+]));
+
+$db->prepare("
+DELETE FROM notifications
+WHERE user_id=?
+")->execute([
+$user['user_id']
+]);
+
+echo json_encode([
+"success"=>true
+]);
